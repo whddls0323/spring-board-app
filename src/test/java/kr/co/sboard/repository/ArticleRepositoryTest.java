db7a7f6991e96062cd95b57a0b7d65cc42647f95
whddls0323 <whddls0323@naver.com>
2025-09-30 12:44:27 +0900

    add 0.0.4

--- src/test/java/kr/co/sboard/repository/ArticleRepositoryTest.java
+++ src/test/java/kr/co/sboard/repository/ArticleRepositoryTest.java
@@ -1,11 +1,11 @@
 package kr.co.sboard.repository;
 
 import com.querydsl.core.Tuple;
+import kr.co.sboard.dto.PageRequestDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -17,10 +17,13 @@ class ArticleRepositoryTest {
 
     @Test
     void test1(){
-        Pageable pageable = PageRequest.of(1, 10);
-        Page<Tuple> pageTuple  = articleRepository.selectArticleAllForList(pageable);
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().build();
+        Pageable pageable = pageRequestDTO.getPageable("ano");
+        Page<Tuple> pageTuple  = articleRepository.selectArticleAllForList(pageRequestDTO, pageable);
+
         List<Tuple> tupleList = pageTuple.getContent();
 
         System.out.println(tupleList);
+
     }
 }
